6f787a55c7d61a9f1c700746bac217389f942440
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-07-11 20:38:48 +0200

    feat: first-module

---
+++ main.tf
@@ -0,0 +1,51 @@
+resource "azurerm_linux_virtual_machine" "" {
+  name                = var.vm_name
+  computer_name       = var.computer_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  size                         = var.size
+  provision_vm_agent           = var.provision_vm_agent
+  proximity_placement_group_id = var.proximity_placement_group_id
+  availability_set_id          = var.availability_set_id
+  network_interface_ids        = var.network_interface_ids
+
+  admin_ssh_key {
+    username   = var.admin_ssh_key_username
+    public_key = var.admin_ssh_key_public_key
+  }
+
+  os_disk {
+    write_accelerator_enabled = var.os_disk_write_accelerator_enabled
+    storage_account_type      = var.os_disk_storage_account_type
+    name                      = var.os_disk_name
+    disk_size_gb              = var.os_disk_disk_size_gb
+    disk_encryption_set_id    = var.os_disk_disk_encryption_set_id
+    caching                   = var.os_disk_caching
+  }
+
+  source_image_reference {
+    publisher = var.source_image_reference_publisher
+    offer     = var.source_image_reference_offer
+    sku       = var.source_image_reference_sku
+    version   = var.source_image_reference_version
+  }
+
+  identity {
+    type = var.identity_type
+  }
+
+  tags = var.tags
+}
+
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "azurerm_ssh_public_key" "this" {
+  name                = var.public_key_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  public_key          = tls_private_key.this.public_key_openssh
+}
